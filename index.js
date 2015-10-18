cb7c550301df88d3eb9d7cdd58fa560d05ff65fa
James <jjsalamon@gmail.com>
2015-10-18 11:53:13 +0800

    no more hoisting D:

--- index.js
+++ index.js
@@ -1,35 +1,28 @@
 var R = require('ramda')
-,	S = require('sanctuary');
+	, S = require('sanctuary');
 var gitter = require('./gitter')
-,	token = require('./token');
+	, token = require('./token')
+	, roomName = require('./roomName') || 'ramda/ramda';
 
 var log = R.tap(console.log);
 
-var roomId = 'ramda/ramda';
 
-listenRoom(roomId);
+var listenRoom = id => {
+	var callback = rooms => {
+		var isRoom = roomName => room => room.name === roomName;
+		var room = rooms.filter(isRoom(id))[0];
+		if (!room)
+			log('nothing found for ' + room.name);
+		log('listening to ' + room.name);
+		gitter.stream(room.id, token, handleStream);
+	};
 
-function listenRoom(id) {
 	gitter.stream(null, token, callback);
-	
-	function callback (rooms) {
-		rooms.map(isRoom(id));
-		
-		function isRoom(roomName) {
-			return function(room) {
-				// console.log(roomName, '===', room.name);
-				if(room.name !== roomName)
-					return
-				console.log('listening to', roomName)
-				gitter.stream(room.id, token, handleStream);
-			}
-		}
-	}
 }
 
+listenRoom(roomName);
 
-
-function handleStream(stream) {
+var handleStream = stream => {
 	try {
 		var username = R.path(['fromUser', 'username']);
 		var message = R.prop('text');
@@ -37,8 +30,8 @@ function handleStream(stream) {
 		var program = R.pipe(output, log);
 		program(stream);
 	}
-	catch(e) {
+	catch (e) {
 		log(e);
 	}
-	
+
 }
